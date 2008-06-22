665310f5e32cde1d229bc0d7a97daba1e31188a9
Ryan Bates <ryan@railscasts.com>
2008-06-22 11:33:36 -0700

    finishing episode 115

--- episode-115/store/app/models/category.rb
+++ episode-115/store/app/models/category.rb
@@ -1,3 +1,7 @@
 class Category < ActiveRecord::Base
   has_many :products
+  
+  def self.all_cached
+    Rails.cache.fetch('Category.all') { all }
+  end
 end
--- episode-115/store/config/environments/production.rb
+++ episode-115/store/config/environments/production.rb
@@ -12,11 +12,15 @@ config.action_controller.consider_all_requests_local = false
 config.action_controller.perform_caching             = true
 config.action_view.cache_template_loading            = true
 
-# Use a different cache store in production
-# config.cache_store = :mem_cache_store
-
 # Enable serving of images, stylesheets, and javascripts from an asset server
 # config.action_controller.asset_host                  = "http://assets.example.com"
 
 # Disable delivery errors, bad email addresses will be ignored
 # config.action_mailer.raise_delivery_errors = false
+
+# Use a different cache store in production
+# config.cache_store = :memory_store
+# config.cache_store = :file_store, '/path/to/cache'
+# config.cache_store = :mem_cache_store
+# config.cache_store = :mem_cache_store, :namespace => 'storeapp'
+# config.cache_store = :mem_cache_store, '123.456.78.9:1001', '123.456.78.9:1002'
