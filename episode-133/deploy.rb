9010d9e3f0c0014fb457c34931be326757032b1f
Ryan Bates <ryan@railscasts.com>
2008-10-25 15:41:13 -0700

    adding desc docs for episode 133

--- episode-133/deploy.rb
+++ episode-133/deploy.rb
@@ -13,15 +13,18 @@ set :repository, "git://github.com/ryanb/railscasts.git"
 set :branch, "master"
 
 namespace :deploy do
+  desc "Tell Passenger to restart the app."
   task :restart do
     run "touch #{current_path}/tmp/restart.txt"
   end
   
+  desc "Symlink shared configs and folders on each release."
   task :symlink_shared do
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
     run "ln -nfs #{shared_path}/assets #{release_path}/public/assets"
   end
   
+  desc "Sync the public/assets directory."
   task :assets do
     system "rsync -vr --exclude='.DS_Store' public/assets #{user}@#{application}:#{shared_path}/"
   end
