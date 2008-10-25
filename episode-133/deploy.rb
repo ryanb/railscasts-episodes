c36c4bf74da0d4ec957138649285243c6f499d53
Ryan Bates <ryan@railscasts.com>
2008-10-25 15:24:36 -0700

    adding episode 133

---
+++ episode-133/deploy.rb
@@ -0,0 +1,30 @@
+set :application, "railscasts.com"
+role :app, application
+role :web, application
+role :db,  application, :primary => true
+
+set :user, "deploy"
+set :deploy_to, "/var/www/apps/#{application}"
+set :deploy_via, :remote_cache
+set :use_sudo, false
+
+set :scm, "git"
+set :repository, "git://github.com/ryanb/railscasts.git"
+set :branch, "master"
+
+namespace :deploy do
+  task :restart do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+  
+  task :symlink_shared do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+    run "ln -nfs #{shared_path}/assets #{release_path}/public/assets"
+  end
+  
+  task :assets do
+    system "rsync -vr --exclude='.DS_Store' public/assets #{user}@#{application}:#{shared_path}/"
+  end
+end
+
+after 'deploy:update_code', 'deploy:symlink_shared'
