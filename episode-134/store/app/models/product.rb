c33a49bd0978c7a1f917c11c096e4bddf8e8121a
Ryan Bates <ryan@railscasts.com>
2008-11-01 21:12:56 -0700

    adding validations to product in episode 134

--- episode-134/store/app/models/product.rb
+++ episode-134/store/app/models/product.rb
@@ -4,4 +4,8 @@ class Product < ActiveRecord::Base
   has_attached_file :photo, :styles => { :small => "150x150>" },
                     :url  => "/assets/products/:id/:style/:basename.:extension",
                     :path => ":rails_root/public/assets/products/:id/:style/:basename.:extension"
+  
+  validates_attachment_presence :photo
+  validates_attachment_size :photo, :less_than => 5.megabytes
+  validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/png']
 end
