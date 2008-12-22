d727ff5d2112bd4bd8775f640283c2772fdac7ee
Ryan Bates <ryan@railscasts.com>
2008-12-22 07:20:48 -0800

    using to_query to convert hash to paypal url in episode 141

--- episode-141/store/app/models/cart.rb
+++ episode-141/store/app/models/cart.rb
@@ -22,6 +22,6 @@ class Cart < ActiveRecord::Base
         "quantity_#{index+1}" => item.quantity
       })
     end
-    "https://www.sandbox.paypal.com/cgi-bin/webscr?"+values.map {|k,v| "#{k}=#{v}" }.join("&")
+    "https://www.sandbox.paypal.com/cgi-bin/webscr?" + values.to_query
   end
 end
