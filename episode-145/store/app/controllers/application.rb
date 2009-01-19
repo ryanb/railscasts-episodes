dbe194b8f6bce2a80e7989c3a0afe4d4decb618c
Ryan Bates <ryan@railscasts.com>
2009-01-19 13:23:44 -0800

    episode 145: filtering card number and verification so they don't show up in logs

--- episode-145/store/app/controllers/application.rb
+++ episode-145/store/app/controllers/application.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
   # See ActionController::Base for details 
   # Uncomment this to filter the contents of submitted sensitive data parameters
   # from your application log (in this case, all fields with names like "password"). 
-  # filter_parameter_logging :password
+  filter_parameter_logging :card_number, :card_verification
   
   def current_cart
     if session[:cart_id]
