b81248cf4f8254d5ee017856a2d9e2c91d0c2ed2
Ryan Bates <ryan@railscasts.com>
2009-03-01 16:19:46 -0800

    adding episode 151

---
+++ episode-151/store/lib/response_timer.rb
@@ -0,0 +1,18 @@
+class ResponseTimer
+  def initialize(app, message = "Response Time")
+    @app = app
+    @message = message
+  end
+  
+  def call(env)
+    @start = Time.now
+    @status, @headers, @response = @app.call(env)
+    @stop = Time.now
+    [@status, @headers, self]
+  end
+  
+  def each(&block)
+    block.call("<!-- #{@message}: #{@stop - @start} -->\n") if @headers["Content-Type"].include? "text/html"
+    @response.each(&block)
+  end
+end
