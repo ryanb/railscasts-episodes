2ed2ebc94e7275120cdc0a21b87d056386f4dbca
Ryan Bates <ryan@railscasts.com>
2009-05-02 17:54:24 -0700

    filtering password from log in episode 160

--- episode-160/blog/app/controllers/application_controller.rb
+++ episode-160/blog/app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   # See ActionController::Base for details 
   # Uncomment this to filter the contents of submitted sensitive data parameters
   # from your application log (in this case, all fields with names like "password"). 
-  # filter_parameter_logging :password
+  filter_parameter_logging :password
   
   helper_method :current_user
   
