4cb9abaf5fcb2dafcf336d3adf47210b6dc19c98
Ryan Bates <ryan@railscasts.com>
2009-05-02 17:38:29 -0700

    adding episode 160

---
+++ episode-160/blog/app/controllers/user_sessions_controller.rb
@@ -0,0 +1,22 @@
+class UserSessionsController < ApplicationController
+  def new
+    @user_session = UserSession.new
+  end
+  
+  def create
+    @user_session = UserSession.new(params[:user_session])
+    if @user_session.save
+      flash[:notice] = "Successfully logged in."
+      redirect_to root_url
+    else
+      render :action => 'new'
+    end
+  end
+  
+  def destroy
+    @user_session = UserSession.find
+    @user_session.destroy
+    flash[:notice] = "Successfully logged out."
+    redirect_to root_url
+  end
+end
---
+++ episode-160/blog/app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:notice] = "Registration successful."
+      redirect_to root_url
+    else
+      render :action => 'new'
+    end
+  end
+  
+  def edit
+    @user = current_user
+  end
+  
+  def update
+    @user = current_user
+    if @user.update_attributes(params[:user])
+      flash[:notice] = "Successfully updated profile."
+      redirect_to root_url
+    else
+      render :action => 'edit'
+    end
+  end
+end
