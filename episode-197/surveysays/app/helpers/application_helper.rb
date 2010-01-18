538f06f92d1a493e8f513895d7f78539681e4ec2
Ryan Bates <ryan@railscasts.com>
2010-01-17 16:48:36 -0800

    updating code for episode 197

--- episode-197/surveysays/app/helpers/application_helper.rb
+++ episode-197/surveysays/app/helpers/application_helper.rb
@@ -1,3 +1,14 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def link_to_remove_fields(name, f)
+    f.hidden_field(:_destroy) + link_to_function(name, "remove_fields(this)")
+  end
+  
+  def link_to_add_fields(name, f, association)
+    new_object = f.object.class.reflect_on_association(association).klass.new
+    fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
+      render(association.to_s.singularize + "_fields", :f => builder)
+    end
+    link_to_function(name, h("add_fields(this, \"#{association}\", \"#{escape_javascript(fields)}\")"))
+  end
 end
--- episode-197/surveysays/public/javascripts/application_jquery.js
+++ episode-197/surveysays/public/javascripts/application_jquery.js
@@ -1,12 +1,10 @@
 function remove_fields(link) {
-  $(link).previous("input[type=hidden]").value = "1";
-  $(link).up(".fields").hide();
+  $(link).prev("input[type=hidden]").val("1");
+  $(link).closest(".fields").hide();
 }
 
 function add_fields(link, association, content) {
   var new_id = new Date().getTime();
   var regexp = new RegExp("new_" + association, "g")
-  $(link).up().insert({
-    before: content.replace(regexp, new_id)
-  });
+  $(link).parent().before(content.replace(regexp, new_id));
 }
