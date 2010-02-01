0d47942afa43f0d51a054ea067c459bca95ed098
Ryan Bates <ryan@railscasts.com>
2010-02-01 02:39:53 -0800

    adding episode 199

---
+++ episode-199/todo/app/controllers/application_controller.rb
@@ -0,0 +1,22 @@
+class ApplicationController < ActionController::Base
+  helper :all
+  protect_from_forgery
+  
+  before_filter :prepare_for_mobile
+  
+  private
+  
+  def mobile_device?
+    if session[:mobile_param]
+      session[:mobile_param] == "1"
+    else
+      request.user_agent =~ /Mobile|webOS/
+    end
+  end
+  helper_method :mobile_device?
+  
+  def prepare_for_mobile
+    session[:mobile_param] = params[:mobile] if params[:mobile]
+    request.format = :mobile if mobile_device?
+  end
+end
