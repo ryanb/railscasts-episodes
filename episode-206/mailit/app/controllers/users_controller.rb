a52c5062f9535ff7d2b84f431de4ab9fd058e93b
Ryan Bates <ryan@railscasts.com>
2010-03-20 18:57:48 -0700

    don't deliver mail in users/show for episode 206

--- episode-206/mailit/app/controllers/users_controller.rb
+++ episode-206/mailit/app/controllers/users_controller.rb
@@ -14,7 +14,6 @@ class UsersController < ApplicationController
   # GET /users/1.xml
   def show
     @user = User.find(params[:id])
-    UserMailer.registration_confirmation(@user).deliver
 
     respond_to do |format|
       format.html # show.html.erb
