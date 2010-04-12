ece998dc5d1ead9f2149687bc4ca02eadc699d10
Ryan Bates <ryan@railscasts.com>
2010-04-12 00:44:54 -0700

    adding episode 209

---
+++ episode-209/todo/app/controllers/projects_controller.rb
@@ -0,0 +1,44 @@
+class ProjectsController < ApplicationController
+  def index
+    @projects = Project.find(:all)
+  end
+  
+  def show
+    @project = Project.find(params[:id])
+  end
+  
+  def new
+    @project = Project.new
+  end
+  
+  def create
+    @project = Project.new(params[:project])
+    if @project.save
+      flash[:notice] = "Successfully created project."
+      redirect_to @project
+    else
+      render :action => 'new'
+    end
+  end
+  
+  def edit
+    @project = Project.find(params[:id])
+  end
+  
+  def update
+    @project = Project.find(params[:id])
+    if @project.update_attributes(params[:project])
+      flash[:notice] = "Successfully updated project."
+      redirect_to @project
+    else
+      render :action => 'edit'
+    end
+  end
+  
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+    flash[:notice] = "Successfully destroyed project."
+    redirect_to projects_url
+  end
+end
