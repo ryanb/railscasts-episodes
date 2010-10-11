edcaa5c6480b5b4321c988511a9b5f4b7ece32e5
Ryan Bates <ryan@railscasts.com>
2010-10-10 23:45:03 -0700

    making I shouldn't check in my Twitter secret, don't worry I reset it

--- episode-235/todo/config/initializers/omniauth.rb
+++ episode-235/todo/config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, 'pwVsMVM3GB9ToV201IqJw', 'qBL6H6yQgi5lvowxs9brR9OmZCZfGvzPEJKfHV4'
+  provider :twitter, 'CONSUMER_KEY', 'CONSUMER_SECRET'
 end
