37ca495f1998306140c0d03e937e4266baa59675
Ryan Bates <ryan@railscasts.com>
2010-12-28 11:42:24 -0800

    episode 246: switching to e.preventDefault instead of return false in jQuery

--- episode-246/store/public/javascripts/application.js
+++ episode-246/store/public/javascripts/application.js
@@ -1,14 +1,13 @@
 if (history && history.pushState) {
-  $(function() {
-    $("#products th a, #products .pagination a").live("click", function() {
+  $(function(e) {
+    $("#products th a, #products .pagination a").live("click", function(e) {
       $.getScript(this.href);
       history.pushState(null, document.title, this.href);
-      return false;
+      e.preventDefault();
     });
     $("#products_search input").keyup(function() {
       $.get($("#products_search").attr("action"), $("#products_search").serialize(), null, "script");
       history.replaceState(null, document.title, $("#products_search").attr("action") + "?" + $("#products_search").serialize());
-      return false;
     });
     $(window).bind("popstate", function() {
       $.getScript(location.href);
