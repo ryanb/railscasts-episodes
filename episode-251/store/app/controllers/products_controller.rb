b482bb8d4da0734196d767f9729caf2035464bac
Ryan Bates <ryan@railscasts.com>
2011-01-30 23:26:23 -0800

    adding episode 251

---
+++ episode-251/store/app/controllers/products_controller.rb
@@ -0,0 +1,42 @@
+class ProductsController < ApplicationController
+  def index
+    @search = Product.search(params[:search])
+    @products = @search.all
+  end
+  
+  def show
+    @product = Product.find(params[:id])
+  end
+  
+  def new
+    @product = Product.new
+  end
+  
+  def create
+    @product = Product.new(params[:product])
+    if @product.save
+      redirect_to @product, :notice => "Successfully created product."
+    else
+      render :action => "new"
+    end
+  end
+  
+  def edit
+    @product = Product.find(params[:id])
+  end
+  
+  def update
+    @product = Product.find(params[:id])
+    if @product.update_attributes(params[:product])
+      redirect_to @product, :notice => "Successfully updated product."
+    else
+      render :action => 'edit'
+    end
+  end
+  
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_url, :notice => "Successfully destroyed product."
+  end
+end
