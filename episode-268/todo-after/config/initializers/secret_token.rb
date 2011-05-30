3fc0fa92d08b5bd0988b77de83c322300e6f93c4
Ryan Bates <ryan@railscasts.com>
2011-05-29 22:15:08 -0700

    adding episode 268

---
+++ episode-268/todo-after/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todo::Application.config.secret_token = 'cb6f4d2f3093bd6571872f008fa658f6688bf1f90d162468821787974ccc4b090c40c81aa752fb3bb374b5b2142628a1bfc7cc8084c8bf27269737d26b6525f0'
