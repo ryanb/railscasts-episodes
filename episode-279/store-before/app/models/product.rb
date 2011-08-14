86c32540e911117451b4d9980187164ed52fda8b
Ryan Bates <ryan@railscasts.com>
2011-08-13 23:08:40 -0700

    adding episode 279

---
+++ episode-279/store-before/app/models/product.rb
@@ -0,0 +1,3 @@
+class Product < ActiveRecord::Base
+  belongs_to :category
+end
---
+++ episode-279/store-before/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Store::Application.config.secret_token = '43d08f1689360bba0ac9596e72901d7807f8ec05ea67446987105e0b85f9e572707bd321672648931f65b86e7847063bcb5ae19cfae1818fe2877e4ba0c38b3b'
