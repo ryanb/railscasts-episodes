c9972d4b3e4ffadcebb5c7b04c72b7ef3da78bfe
Ryan Bates <ryan@railscasts.com>
2011-10-24 19:45:03 -0700

    adding episodes 292 and 293

---
+++ episode-293/todo-after/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todo::Application.config.secret_token = '358f58a7287f2e8166595e39d324a97a66a4ce9ab0725d7430ae6524d4b9bc4777389c1a3cb2486016fbdca5d7daad16c6da2076ace35e7fda4d4850da710f4b'
---
+++ episode-293/todo-after/config/unicorn.rb
@@ -0,0 +1,8 @@
+working_directory "/vagrant"
+pid "/vagrant/tmp/pids/unicorn.pid"
+stderr_path "/vagrant/log/unicorn.log"
+stdout_path "/vagrant/log/unicorn.log"
+
+listen "/tmp/unicorn.todo.sock"
+worker_processes 2
+timeout 30
