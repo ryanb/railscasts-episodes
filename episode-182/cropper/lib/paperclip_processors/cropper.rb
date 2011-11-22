4efd96b40511c63df3ab25b917dfad618211c332
Rishi Shah <bobbungee@gmail.com>
2011-11-21 19:42:15 -0600

    Command injection fix

--- episode-182/cropper/lib/paperclip_processors/cropper.rb
+++ episode-182/cropper/lib/paperclip_processors/cropper.rb
@@ -11,7 +11,7 @@ module Paperclip
     def crop_command
       target = @attachment.instance
       if target.cropping?
-        " -crop '#{target.crop_w}x#{target.crop_h}+#{target.crop_x}+#{target.crop_y}'"
+        " -crop '#{target.crop_w.to_i}x#{target.crop_h.to_i}+#{target.crop_x.to_i}+#{target.crop_y.to_i}'"
       end
     end
   end
