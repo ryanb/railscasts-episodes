5d6c5c02a9ebda1a86f244c7e46c47a54cabf481
Raison D'souza <raisondsouza@gmail.com>
2012-03-28 11:56:06 +0530

    Twitter response structure changed .. user_info becomes info

--- episode-241/blog/app/models/user.rb
+++ episode-241/blog/app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
     create! do |user|
       user.provider = auth["provider"]
       user.uid = auth["uid"]
-      user.name = auth["user_info"]["name"]
+      user.name = auth["info"]["name"]
     end
   end
 end
